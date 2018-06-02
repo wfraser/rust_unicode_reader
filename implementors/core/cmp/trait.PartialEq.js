14bd1565c5859218d44f72ddbc935985f6f6200c
Travis CI <noreply@travis-ci.org>
2018-06-02 00:07:09 +0000

    Documentation generated for 57c57a8ad608d9c99835da4e03a34d76111aec60 ()

--- implementors/core/cmp/trait.PartialEq.js
+++ implementors/core/cmp/trait.PartialEq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["unicode_segmentation"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"unicode_segmentation/enum.GraphemeIncomplete.html\" title=\"enum unicode_segmentation::GraphemeIncomplete\">GraphemeIncomplete</a>",];
+implementors["unicode_segmentation"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"unicode_segmentation/enum.GraphemeIncomplete.html\" title=\"enum unicode_segmentation::GraphemeIncomplete\">GraphemeIncomplete</a>",synthetic:false,types:["unicode_segmentation::grapheme::GraphemeIncomplete"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
