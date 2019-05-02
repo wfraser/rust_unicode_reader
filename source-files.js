b827cce296b224cc931924e260e5d43e6f73d192
Travis CI <noreply@travis-ci.org>
2019-05-02 00:12:26 +0000

    Documentation generated for 57c57a8ad608d9c99835da4e03a34d76111aec60 ()

--- source-files.js
+++ source-files.js
@@ -1,3 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["unicode_reader"] = {"name":"","dirs":[],"files":["codepoints.rs","graphemes.rs","lib.rs"]};
 sourcesIndex["unicode_segmentation"] = {"name":"","dirs":[],"files":["grapheme.rs","lib.rs","tables.rs","word.rs"]};
+createSourceSidebar();
